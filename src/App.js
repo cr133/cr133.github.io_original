00c07176c28e73bedf64a35c0015121242c5b165
Gareth-CR-Lee <real.gareth.lee@gmail.com>
2018-10-01 09:30:43 -0400

    GL absolutely completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import React from 'react';
-import Canvas from './gl/gl_main';
+import Canvas, { circle, getFilledCirclePixels } from './gl/gl_main';
 
 class App extends React.Component {
-    notification() {
-        window.onload = function() {
-            window.setTimeout(() => {
-                // Work this
-                const notitext = document.createElement('h3');
-                notitext.style.color = "white";
-                notitext.style.position = "absolute";
-                notitext.style.left = "40%";
-                notitext.style.top = "60%";
-                notitext.innerHTML = "Click the middle circle to find more";
-                document.body.appendChild(notitext);
-            }, 2000);
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            rend: 0
+        };
+        this.updateValue = this.updateValue.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.rend !== nextState.rend)
+            return true;
+        return false;
+    }
+    updateValue(e) {
+        if (this.state.rend === 0 && getFilledCirclePixels(circle.x, circle.y, e.clientX, e.clientY, circle.r)) {
+            this.setState(() => ({
+                rend: 1
+            }))
+        } else {
+            this.setState(() => ({
+                rend: 0
+            }))
         }
     }
 
     render() {
-        {this.notification()}
-        return (
-            <div id="app">
-                <h1 id="target">React + WebGL</h1>
-                <Canvas />
-            </div>
-        )
+        if (this.state.rend === 0) {
+            return (
+                <div id="app" onClick={this.updateValue}>
+                    {/* TODO: Replace h1 with a component */}
+                    <h1>Main page</h1>
+                    <Canvas />
+                </div>
+            )
+        } else {
+            return (
+                <div id="app" onDoubleClick={this.updateValue}>
+                    {/* TODO: Replace h1 with a component */}
+                    <h1>Next page</h1>
+                    <Canvas />
+                </div>
+            )
+        }
     }
 }
 
--- src/gl/gl_main.js
+++ src/gl/gl_main.js
@@ -3,13 +3,20 @@ import vert from './vert';
 import frag from './frag';
 import { resizeCanvas } from './util';
 import * as glm from './lib/gl-matrix';
+import * as App from '../App';
 
 let rotate_value = 0.0;
 let shouldRotate = true;
 let scale_value = 1;
+let shouldShrink = false;
+let fade_in_value = 0.0;
+let shouldFade = false;
+let expand_value = 0.0;
+let shouldExpand = false;
+
+export const circle = { x: 0, y: 0, r: 0 }
 
 export function Main(gl) {
-    mouse(gl);
     // Create a shader
     const shaderProgram = loadProgram(gl, vert, frag);
 
@@ -27,18 +34,11 @@ export function Main(gl) {
     gl.useProgram(programInfo.program);
 
     // Set vertices and colors
-
     let vertices = [];
-    let colors = [];
 
     for (let i = 0; i < 360; i++) {
         vertices.push(Math.cos((i * Math.PI / 180.0)));
         vertices.push(Math.sin((i * Math.PI / 180.0)));
-
-        colors.push(0.92941);
-        colors.push(0.94902);
-        colors.push(0.76078);
-        colors.push(1.0);
     }
 
     // Send to GPU
@@ -52,6 +52,43 @@ export function Main(gl) {
     gl.vertexAttribPointer(programInfo.aLocation.vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.aLocation.vPosition);
 
+    // place of setting color, if no animation is required
+
+    let then = 0.0;
+
+    function render(now) {
+        now *= 0.001;
+        const deltaTime = now - then;
+        then = now;
+
+        mouse(gl);
+
+        drawScene(gl, programInfo, deltaTime);
+
+        requestAnimationFrame(render);
+    } 
+    requestAnimationFrame(render);
+}
+
+// For update, put here. Otherwise, put Main
+const drawScene = (gl, programInfo, deltaTime) => {
+    // color
+    let colors = [];
+
+    if (shouldFade && fade_in_value > -0.2) {
+        fade_in_value -= 0.01;
+    }
+    else if (!shouldFade && fade_in_value < 0.0) {
+        fade_in_value += 0.004;
+    }
+
+    for (let i = 0; i < 360; i++) {
+        colors.push(0.92941 + fade_in_value);
+        colors.push(0.94902 + fade_in_value);
+        colors.push(0.76078 + fade_in_value);
+        colors.push(1.0);
+    }
+
     const fBuffers = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, fBuffers);
     gl.bufferData(
@@ -62,21 +99,6 @@ export function Main(gl) {
     gl.vertexAttribPointer(programInfo.aLocation.vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.aLocation.vColor);
 
-    let then = 0.0;
-
-    function render(now) {
-        now *= 0.001;
-        const deltaTime = now - then;
-        then = now;
-
-        drawScene(gl, programInfo, deltaTime);
-
-        requestAnimationFrame(render);
-    } 
-    requestAnimationFrame(render);
-}
-
-const drawScene = (gl, programInfo, deltaTime) => {
     // Clear background
     resizeCanvas(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -93,78 +115,109 @@ const drawScene = (gl, programInfo, deltaTime) => {
     let rotate = glm.vec3.create();
     let scale = glm.vec3.create();
 
-    glm.vec3.set(scale, 0.16 * scale_value, 0.16 * scale_value, 1.0);
+    if (shouldShrink && scale_value > 0.95) {
+        scale_value -= 0.002;
+    }
+    else if (!shouldShrink && scale_value < 1.0) {
+        scale_value += 0.002;
+    }
+
+    if (shouldExpand && expand_value < 1.4) {
+        expand_value += 0.1;
+    }
+    else if (!shouldExpand && expand_value > 0.0) {
+        expand_value -= 0.1;
+    }
+    if (expand_value < 0.0)
+        expand_value = 0.0;
+
+    glm.vec3.set(scale, 0.16 * scale_value + expand_value, 0.16 * scale_value + expand_value, 1.0);
     
     glm.mat4.scale(modelViewMatrix, modelViewMatrix, scale);
 
     gl.uniformMatrix4fv(programInfo.uLocation.modelView, false, modelViewMatrix);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 360);
 
-    // Draw bigger circle
-    modelViewMatrix = glm.mat4.create();
-    glm.vec3.set(translate, 0.6, 0.0, 0.0);
-    glm.vec3.set(rotate, 0.0, 0.0, 1.0);
-    glm.vec3.set(scale, 0.08, 0.08, 1.0);
-
-    glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
-    glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
-    glm.mat4.rotate(modelViewMatrix, modelViewMatrix, 2 * rotate_value, rotate);
-    glm.mat4.scale(modelViewMatrix, modelViewMatrix, scale);
-
-    gl.uniformMatrix4fv(programInfo.uLocation.modelView, false, modelViewMatrix);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 360);
-
-    // Draw smaller circle
-    modelViewMatrix = glm.mat4.create();
-    glm.vec3.set(scale, 0.04, 0.04, 1.0);
+    // If the biggest circle is expanded, or the rest will disappear
+    if (!shouldExpand) {
+        // Draw bigger circle
+        modelViewMatrix = glm.mat4.create();
+        glm.vec3.set(translate, 0.6, 0.0, 0.0);
+        glm.vec3.set(rotate, 0.0, 0.0, 1.0);
+        glm.vec3.set(scale, 0.08, 0.08, 1.0);
     
-    glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
-    glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
-    glm.mat4.rotate(modelViewMatrix,modelViewMatrix, -4 * rotate_value, rotate);
-    glm.vec3.set(translate, 0.2, 0.0, 0.0);
-    glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
-    glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
-    glm.mat4.scale(modelViewMatrix, modelViewMatrix, scale);
+        glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
+        glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
+        glm.mat4.rotate(modelViewMatrix, modelViewMatrix, 2 * rotate_value, rotate);
+        glm.mat4.scale(modelViewMatrix, modelViewMatrix, scale);
+    
+        gl.uniformMatrix4fv(programInfo.uLocation.modelView, false, modelViewMatrix);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 360);
+    
+        // Draw smaller circle
+        modelViewMatrix = glm.mat4.create();
+        glm.vec3.set(scale, 0.04, 0.04, 1.0);
+        
+        glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
+        glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
+        glm.mat4.rotate(modelViewMatrix,modelViewMatrix, -4 * rotate_value, rotate);
+        glm.vec3.set(translate, 0.2, 0.0, 0.0);
+        glm.mat4.translate(modelViewMatrix, modelViewMatrix, translate);
+        glm.mat4.rotate(modelViewMatrix, modelViewMatrix, rotate_value, rotate);
+        glm.mat4.scale(modelViewMatrix, modelViewMatrix, scale);
+    
+        gl.uniformMatrix4fv(programInfo.uLocation.modelView, false, modelViewMatrix);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 360);
 
-    gl.uniformMatrix4fv(programInfo.uLocation.modelView, false, modelViewMatrix);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 360);
-
-    // rotate animation
-    if (shouldRotate)
-    rotate_value += deltaTime;
+        // rotate animation
+        if (shouldRotate)
+            rotate_value += deltaTime;
+    }
 }
 
-////////////////////////////////
+// Mouse events
 const mouse = (gl) => {
-    const circle = {
-        x: Math.floor(window.innerWidth / 2),
-        y: Math.floor(window.innerHeight / 2),
-        r: Math.floor(gl.canvas.clientWidth / 2 * 0.16)
-    };
+    
+    circle.x = Math.floor(window.innerWidth / 2);
+    circle.y = Math.floor(window.innerHeight / 2);
+    circle.r = Math.floor(gl.canvas.clientWidth / 2 * 0.16);
     
     gl.canvas.onmousemove = function(e) {
         if (getFilledCirclePixels(circle.x, circle.y, e.clientX, e.clientY, circle.r)) {
-            // 1. Color change
-            shouldRotate = false;
-            scale_value = 0.95;
+            if (!shouldExpand) {
+                shouldRotate = false;
+                shouldFade = true;
+                shouldShrink = true;
+            }
         }
         else {
             shouldRotate = true;
-            scale_value = 1;
+            shouldFade = false;
+            shouldShrink = false;
         }
     }
     
     gl.canvas.onclick = function(e) {
         if (getFilledCirclePixels(circle.x, circle.y, e.clientX, e.clientY, circle.r)) {
-            // 1. Scaling the circle
-            // 2. Go to the next page
-            const target = document.getElementById('target');
-            target.style.color = "lightgreen";
+            shouldExpand = true;
+            shouldFade = false;
+            gl.canvas.style.width = "100vw";
+            gl.canvas.style.height = "100vh";
         }
+    }
+
+    gl.canvas.ondblclick = function(e) {
+        shouldExpand = false;
+    }
+
+    if (!shouldExpand && expand_value < 0.8) {
+        gl.canvas.style.width = "100vmin";
+        gl.canvas.style.height = "100vmin";
     }
 }
 
-const getFilledCirclePixels = (x, y, ex, ey, radius) => {
+// Borrowed this algorithm, called Bresenham algorithm
+export const getFilledCirclePixels = (x, y, ex, ey, radius) => {
     let xoff = 0;
     let yoff = radius;
     let bal = -radius;
@@ -199,9 +252,7 @@ const line = (x, y, ex, ey, w) => {
     return false;
 }
 
-
-//////////////////////////////////
-
+// Shader configuration
 const loadProgram = (gl, vert, frag) => {
     const vsrc = loadShader(gl, gl.VERTEX_SHADER, vert);
     const fsrc = loadShader(gl, gl.FRAGMENT_SHADER, frag);
